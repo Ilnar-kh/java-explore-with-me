af7bf239d6499c6d745303d1fe928a058362b57f
Ilnar Khusainov <ilnarkhusainov@MacBook-Air-Ilnar.local>
2025-12-20 14:54:30 -0800

    main_svc

--- ewm-main-service/src/main/java/ru/practicum/main/util/DateTimeUtils.java
+++ ewm-main-service/src/main/java/ru/practicum/main/util/DateTimeUtils.java
@@ -6,7 +6,8 @@ import java.time.format.DateTimeFormatter;
 public final class DateTimeUtils {
     private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    private DateTimeUtils() {}
+    private DateTimeUtils() {
+    }
 
     public static LocalDateTime parse(String value) {
         return LocalDateTime.parse(value, FORMATTER);
